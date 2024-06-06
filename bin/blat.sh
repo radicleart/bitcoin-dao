071267b1ad3f621476dcc9bbacd2f0d716509ee5
Mike Cohen <mjoecohen@gmail.com>
2024-06-06 10:54:32 +0100

    script update - finding clarinet faulty on mac and exploring cleaning up packages managed by brew

--- bin/blat.sh
+++ bin/blat.sh
@@ -3,3 +3,8 @@
 sh -c "truncate -s 0 /var/lib/docker/containers/**/*-json.log"
 docker system prune -a -f
 docker volume rm $(docker volume ls -qf dangling=true)
+
+
+# see also
+# brew upgrade
+# brew cleanup --prune=all
